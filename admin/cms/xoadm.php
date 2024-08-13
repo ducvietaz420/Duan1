9d458fb545f7d989391a56ed3502fdc187af7edb
ducvietaz420 <ducvietaz420@gmail.com>
2024-08-13 17:10:27 +0700

    xoa code khong lien quan

--- admin/cms/xoadm.php
+++
@@ -1,8 +0,0 @@
-<?php
-     $ma=$_REQUEST['ma'];
-     $query= "delete from danhmuc where madm=$ma";
-     $count=$db->exec($query);
-     if($count>0)
-         header("location:?page=dsdm");
-?>
-
